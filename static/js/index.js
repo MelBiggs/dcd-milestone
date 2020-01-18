408e0eff25ca0f3e444da1ae2656b4f74b35596a
MelBiggs <melissabiggs16@gmail.com>
2020-01-18 19:54:37 +0000

    Added steps for recipes - add and remove

--- static/js/index.js
+++ static/js/index.js
@@ -12,11 +12,21 @@ $(document).ready(function(){
         var newthing = $(".ingredient-list-item:first").clone();      
         newthing.insertBefore(this)
         newthing.find("input").val("");
-        $("<a>").insertAfter(newthing.find("input")).attr("class","remove-ingredients-btn").text("remove");
+        $("<a>").insertAfter(newthing.find("input")).attr("class","remove-btn").text("remove");
     });
 
-    $(document).on("click", ".remove-ingredients-btn", function (e) {
+    $(document).on("click", ".remove-btn", function (e) {
         e.preventDefault();  
         this.closest("div").remove();
+    });
+
+    $("#add-step").on("click", function (e) {
+        // Prevent Default stops the form being submitted after this button is clicked 
+        e.preventDefault();  
+        
+        var newthing = $(".step-list-item:first").clone();      
+        newthing.insertBefore(this)
+        newthing.find("input").val("");
+        $("<a>").insertAfter(newthing.find("input")).attr("class","remove-btn").text("remove");
     });
 });
